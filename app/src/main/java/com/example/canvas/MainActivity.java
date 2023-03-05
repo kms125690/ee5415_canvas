5200069b9e20084317f122e7eaefd582049f3229
KMS <kms125690@gmail.com>
2023-03-05 23:26:18 +0800

    init

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,25 +25,34 @@ public class MainActivity extends AppCompatActivity {
             super(context);
         }
         protected void onDraw(Canvas canvas) {
+//            canvas.scale(0.8f, 0.8f);
             int width = getWidth();
             int height = getHeight();
             Paint linePaint = new Paint();
             linePaint.setColor(Color.GRAY);
-            linePaint.setStrokeWidth(2);
-            for (int x = 0; x < width; x += width/5)
-                canvas.drawLine(x, 0, x, height, linePaint);
-            for (int y = 0; y < height; y += height/5)
-                canvas.drawLine(0, y, width, y, linePaint);
+            linePaint.setStrokeWidth(4);
+            canvas.drawLine(200.0f, 200.0f, 200.0f, height - 200.0f, linePaint);
+            canvas.drawLine(width - 200.0f, 200.0f, width - 200.0f, height - 200.0f, linePaint);
+//            for (int x = 0; x < width; x += width/5)
+//                canvas.drawLine(x, 0, x, height, linePaint);
+
+            canvas.drawLine(200.0f, 200.0f, width - 200.0f, 200.0f, linePaint);
+            canvas.drawLine(200.0f, height - 200.0f, width - 200.0f, height - 200.0f, linePaint);
+//            for (int y = 0; y < height; y += height/5)
+//                canvas.drawLine(0, y, width, y, linePaint);
+
             Paint circlePaint = new Paint();
-            circlePaint.setColor(Color.argb(255, 0, 100, 200));
-            canvas.drawCircle(width/2, height/2, width/4, circlePaint);
+            circlePaint.setColor(Color.argb(255, 255, 0, 0));
+            canvas.drawCircle(width/2, height/2, width / 5, circlePaint);
+
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.icon_128);
-            canvas.drawBitmap(bitmap, (width - bitmap.getWidth()) / 2, 80, null);
+            canvas.drawBitmap(bitmap, (width - bitmap.getWidth()) / 2, 300, null);
+
             Paint textPaint = new Paint();
-            textPaint.setColor(Color.RED);
-            textPaint.setTextSize(40);
+            textPaint.setColor(Color.GREEN);
+            textPaint.setTextSize(120);
             textPaint.setTextAlign(Paint.Align.CENTER);
-            canvas.drawText("ANDROID", width/2, height*3/4, textPaint);
+            canvas.drawText("BMI", width/2, height*3/4, textPaint);
         }
     }
 }
